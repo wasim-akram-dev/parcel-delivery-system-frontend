1ceb8e475c1f8384a12c500433e4a53f6e847be2
Washim-Akram <muhammadwashimakram@gmail.com>
2025-11-03 17:13:12 +0600

    Redux setup done

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router/dom";
 import "./index.css";
+import { store } from "./redux/store";
 import router from "./routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <ReduxProvider store={store}>
+      <RouterProvider router={router} />
+    </ReduxProvider>
   </StrictMode>
 );
