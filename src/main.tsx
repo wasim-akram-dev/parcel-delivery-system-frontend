f2949f155ccc16f0da605800596e447eab8229e7
Washim-Akram <muhammadwashimakram@gmail.com>
2025-11-04 14:14:27 +0600

    add theme provider and modified footer

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router/dom";
+import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
+import { ThemeProvider } from "./providers/theme-provider";
 import { store } from "./redux/store";
 import router from "./routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider store={store}>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </ReduxProvider>
   </StrictMode>
 );
